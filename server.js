9160291a133e4aa2156e796700de622f3e7c23e6
dgao <delong@tyemill.com>
2014-03-06 00:42:33 -0800

    add router: modulized idea is still brillient. One shining thing is that denpendency injection is actually done though passing functions around. More will be found in the comments.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
 	function onRequest(request, response) {
-		console.log("request received");
-		response.writeHead(200, { "Content-Type": "text/plain" });
+        var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received");
+
+        route(pathname);
+
+        response.writeHead(200, { "Content-Type": "text/plain" });
 		response.write("Hello World");
 		response.end();
 	}
